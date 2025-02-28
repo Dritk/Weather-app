423968c935f5624652bba79db71f3a9806860523
Dritk <dritkarmacharya@gmail.com>
2025-02-28 15:57:46 +0545

    Aqi update

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -3,7 +3,7 @@ import {
   ForecastDataProps,
   AqiDataProps,
 } from "../types/types";
-
+//Api KEy
 const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 const API_URL = import.meta.env.VITE_WEATHER_API_URL;
 
